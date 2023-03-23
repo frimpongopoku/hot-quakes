5c7be343f397f0e2266d3422e14172bad199756a
frimpongOpoku <pongofrimi@gmail.com>
2023-03-23 20:18:26 +0400

    bottom sheet is working as needed

--- app/src/main/java/com/frimpong/hot_quakes/DetailsActivity.java
+++ app/src/main/java/com/frimpong/hot_quakes/DetailsActivity.java
@@ -3,6 +3,9 @@ package com.frimpong.hot_quakes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        FrameLayout bottomSheet = findViewById(R.id.bottom_sheet);
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+
+        bottomSheetBehavior.setPeekHeight(200); // Initial height of bottom sheet when docked
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED); // Bottom sheet is collapsed by default
+
     }
 }
