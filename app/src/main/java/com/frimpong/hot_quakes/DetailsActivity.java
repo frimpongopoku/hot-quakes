37969cdb3b12a30fbbc0b01d3d1e7547e1045f83
frimpongOpoku <pongofrimi@gmail.com>
2023-03-23 21:56:34 +0400

    added api key for maps, but it looks like its not working yet, ago wait abit

--- app/src/main/java/com/frimpong/hot_quakes/DetailsActivity.java
+++ app/src/main/java/com/frimpong/hot_quakes/DetailsActivity.java
@@ -5,19 +5,68 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.FrameLayout;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    private MapView mapView;
+    private GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        FrameLayout bottomSheet = findViewById(R.id.bottom_sheet);
-        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
 
-        bottomSheetBehavior.setPeekHeight(200); // Initial height of bottom sheet when docked
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED); // Bottom sheet is collapsed by default
 
+        // Initialize the map view
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+
+        // Get the GoogleMap object
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+                // Add a marker to the map at a specific location
+                LatLng location = new LatLng(37.7749, -122.4194);
+                MarkerOptions markerOptions = new MarkerOptions()
+                        .position(location)
+                        .title("Marker Title");
+                map.addMarker(markerOptions);
+                // Move the camera to the marker location
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f));
+            }
+        });
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
 }
