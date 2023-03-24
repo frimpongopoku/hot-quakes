fa8f6284363de8e058671c153d13c3edf65a52b5
frimpongOpoku <pongofrimi@gmail.com>
2023-03-24 17:34:22 +0400

    been able to add teh searchbox and datetimepicker, but not been able to change the color of the searchview

--- app/src/main/java/com/frimpong/hot_quakes/MainActivity.java
+++ app/src/main/java/com/frimpong/hot_quakes/MainActivity.java
@@ -1,13 +1,23 @@
 package com.frimpong.hot_quakes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.SearchView;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,17 +26,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeToolbar();
+        setupRecyclerView();
+
+    }
+
+    public void setupRecyclerView(){
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         final EarthquakeListAdapter adapter = new EarthquakeListAdapter(this);
         recyclerView.setAdapter(adapter);
 
-         EarthquakeViewModel earthquakeViewModel = new ViewModelProvider(this).get(EarthquakeViewModel.class);
+        EarthquakeViewModel earthquakeViewModel = new ViewModelProvider(this).get(EarthquakeViewModel.class);
         earthquakeViewModel.getEarthquakes().observe(this, new Observer<List<EarthquakeItem>>() {
             @Override
             public void onChanged(List<EarthquakeItem> earthquakes) {
                 adapter.setEarthquakes(earthquakes);
+            }
+        });
+    }
+
+
+    private void showDatePickerDialog() {
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                // Handle the selected date
+            }
+        }, year, month, day);
+        datePickerDialog.show();
+    }
+
+
+
+    public void  initializeToolbar(){
+        // Initialize the Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Set the click listener for the date picker button
+        ImageButton datePickerButton = (ImageButton) findViewById(R.id.date_picker_button);
+        datePickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
             }
         });
     }
